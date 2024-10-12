90608f615d2a47d0fbce44b3d5f0463864f19853
Bakhriev <muradbakhriev@gmail.com>
2024-10-12 12:18:29 +0500

    .

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,34 @@
-const burger = document.querySelector('.burger');
-const mobileMenu = document.querySelector('.mobile-menu');
+const menuInit = () => {
+	const burger = document.querySelector('.burger');
+	const mobileMenu = document.querySelector('.mobile-menu');
 
-burger.addEventListener('click', () => {
-	burger.classList.toggle('active');
-	mobileMenu.classList.toggle('active');
-});
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('active');
+		mobileMenu.classList.toggle('active');
+	});
+};
+
+const tabInit = () => {
+	const tabNavs = document.querySelectorAll('.tab__nav button');
+	const tabItems = document.querySelectorAll('.tab__content .tab__item');
+
+	if (!tabNavs.length || !tabItems.length) return;
+
+	tabNavs[0].classList.add('active');
+	tabItems[0].classList.add('active');
+
+	tabNavs.forEach(nav => {
+		nav.addEventListener('click', () => {
+			if (!nav.classList.contains('active')) {
+				tabNavs.forEach(item => item.classList.remove('active'));
+				tabItems.forEach(item => item.classList.remove('active'));
+
+				const index = nav.dataset.tab;
+				nav.classList.add('active');
+				tabItems[index].classList.add('active');
+			}
+		});
+	});
+};
+
+tabInit();
